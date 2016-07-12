d0a4b9efdc0f561f7b57a2aa74f637f61b3058bb
CMattoxASC <colemattox17@gmail.com>
2016-07-12 11:39:56 -0400

    Variable Square
    Square using variable

---
+++ Python Lesson 1.py
@@ -0,0 +1,4 @@
+All = 9
+myFancyVariable = All
+s1tuff = 5.0
+print(myFancyVariable)
---
+++ Variable Square.py
@@ -0,0 +1,10 @@
+from Myro import *
+init(sim)
+size = 5
+forward(1, size)
+turnBy(90)
+forward(1, size)
+turnBy(90)
+forward(1, size)
+turnBy(90)
+forward(1, size)

a15e4785a44b08da80496e6d0d6f02aeb341773e
CMattoxASC <colemattox17@gmail.com>
2016-07-13 09:21:05 -0400

    Looping
    While and Square Loops

---
+++ While I and J Loop.py
@@ -0,0 +1,8 @@
+i = 0
+while i < 5:
+    print("variable 'i' is ", i)
+    j = 0
+    while j < 3:
+        print("variable 'j' is: ", j)
+        j = j + 1
+    i = i + 1
---
+++ While Loop.py
@@ -0,0 +1,6 @@
+myVariable = "All Star Code"
+i = 0
+while i < 10:
+    print(i)
+    print(myVariable)
+    i = i + 1

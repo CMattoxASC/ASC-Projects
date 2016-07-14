dfa2b81abbffa677493dafd53ae5b7f431afb26f
CMattoxASC <colemattox17@gmail.com>
2016-07-14 10:30:30 -0400

    Conditionals
    Conditional Examples

---
+++ Conditionals.py
@@ -0,0 +1,24 @@
+def goZoo():
+    print("You went to the zoo")
+    
+def goManhattan():
+    print("You went to manhattan")
+    
+def callMommy():
+    print("You called Mommy")    
+
+goBronx = False
+goManhattan = True
+
+goBronx = True
+if goBronx:
+    gozoo()
+
+elif timeSquare:
+    goManhattan:
+
+else:
+    callMommy()
+        
+    
+    
---
+++ Score Conditionals.py
@@ -0,0 +1,15 @@
+score = 85
+        
+if score < 60:
+    print("You failed")
+
+elif score < 80:
+    print("You did alight")
+    
+elif score < 90:
+    print("You did pretty well")
+    
+else:
+    print("You did great!")            
+                                          
+    
